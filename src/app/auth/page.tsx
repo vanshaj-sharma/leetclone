5c96e53b00790db546cfe8b29fa7ca9a8ad02a1e
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-07-18 16:55:41 +0530

    started creating authenticaiton models, create AuthModal

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,4 @@
+import AuthModal from "@/components/Modals/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
 
 type AuthPageProps = {};
@@ -7,6 +8,10 @@ const AuthPage: React.FC<AuthPageProps> = () => {
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
         <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none text-white">
+          {/* {//add some photo here} */}
+        </div>
+        <AuthModal />
       </div>
     </div>
   );
