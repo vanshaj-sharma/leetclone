8474e22c2ffd0430db3e8a7b58fd39bf829c20f3
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-08-16 11:57:38 +0530

    problems table ui implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ProblemsTable from "./components/ProblemsTable/ProblemsTable";
 import Topbar from "./components/Topbar/Topbar";
 
 export default function Home() {
@@ -30,6 +31,7 @@ export default function Home() {
               </th>
             </tr>
           </thead>
+          <ProblemsTable />
         </table>
       </div>
     </main>
