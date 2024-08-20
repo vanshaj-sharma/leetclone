1e2aa5a1fc5d8c0495b854503238b5fefd1508a3
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-08-20 12:10:53 +0530

    added reset password functionality

--- src/app/components/Modals/ResetPassword.tsx
+++ src/app/components/Modals/ResetPassword.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { auth } from "@/app/firebase/firebase";
+import React, { useEffect, useState } from "react";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
 
 type ResetPasswordProps = {};
 
 const ResetPassword: React.FC<ResetPasswordProps> = () => {
+  const [email, setEmail] = useState("");
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
+  const handleReset = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const success = await sendPasswordResetEmail(email);
+    if (success) {
+      alert("Sent email");
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert(error.message);
+    }
+  }, []);
+
   return (
-    <form className="space-y-6 px-6 pb-4 sm:pb-6 xl:pb-8">
+    <form
+      className="space-y-6 px-6 pb-4 sm:pb-6 xl:pb-8"
+      onSubmit={handleReset}
+    >
       <h3 className="text-xl font-medium text-white ">Reset Password</h3>
       <p className="text-sm text-white">
         Forgotten your password? Enter you e-mail address below, and we&apos;II
@@ -21,6 +43,7 @@ const ResetPassword: React.FC<ResetPasswordProps> = () => {
           type="email"
           name="email"
           id="email"
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter Your Email"
           className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
          bg-gray-600 border-gray-500 placeholder-gray-400 text-white "
